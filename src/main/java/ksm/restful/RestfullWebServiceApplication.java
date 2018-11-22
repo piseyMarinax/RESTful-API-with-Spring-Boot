76decd2f948e8c7dab6b40ab49fcf040f8517a38
pisey <pisey@marinax.biz>
2018-11-22 14:20:23 +0700

    first commit

---
+++ src/main/java/ksm/restful/RestfullWebServiceApplication.java
@@ -0,0 +1,12 @@
+package ksm.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullWebServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/ksm/restful/RestfullWebServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ksm.restful;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfullWebServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
